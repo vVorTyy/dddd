54cadf993f1fc75a68bdbd673a955af82e47d124
vVorTyy <alabdali1333@gmail.com>
2025-03-12 15:27:16 +0300

    Initial commit

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 function download_pdf() {
+    const { jsPDF } = window.jspdf; // استدعاء jsPDF بشكل صحيح
+
     const pdfContent = document.getElementById("all");
 
-    html2pdf()
-        .set({
-            margin: 0,
-            filename: "lost_and_found_report.pdf",
-            image: { type: 'jpeg', quality: 1 },
-            html2canvas: { scale: 2 },
-            jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
-        })
-        .from(pdfContent)
-        .save();
-}
+    html2canvas(pdfContent, { scale: 2 }).then(canvas => {
+        const imgData = canvas.toDataURL("image/jpeg", 1.0);
+        const pdf = new jsPDF({
+            orientation: "portrait",
+            unit: "mm",
+            format: "tabloid"
+        });
 
+        const imgWidth = 280;
+        const pageHeight = 500;
+        const imgHeight = (canvas.height * imgWidth) / canvas.width;
+        let position = 0;
+
+        pdf.addImage(imgData, "JPEG", 0, position, imgWidth, imgHeight);
+
+        if (imgHeight > pageHeight) {
+            while (position + pageHeight < imgHeight) {
+                position += pageHeight;
+                pdf.addPage();
+                pdf.addImage(imgData, "JPEG", 0, -position, imgWidth, imgHeight);
+            }
+        }
+
+        pdf.save("lost_and_found_report.pdf");
+    });
+}
