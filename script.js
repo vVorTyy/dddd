a84fdc452f531947644bec5ea73f320d55ffd43e
vVorTyy <alabdali1333@gmail.com>
2025-03-12 19:12:11 +0300

    frist

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function download_pdf() {
             filename: "lost_and_found_report.pdf",
             image: { type: 'jpeg', quality: 0.7 },
             html2canvas: { scale: 4 },
-            jsPDF: { unit: 'mm', format: [155, 105], orientation: 'portrait' } // الأبعاد مقسمة على 2
+            jsPDF: { unit: 'mm', format: [170, 105], orientation: 'portrait' } // الأبعاد مقسمة على 2
         })
         .from(pdfContent)
         .save();
