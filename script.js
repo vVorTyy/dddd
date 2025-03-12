01450ffcd879b171d97f8e173a88a1ed915166c4
vVorTyy <alabdali1333@gmail.com>
2025-03-12 19:18:39 +0300

    frisst

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function download_pdf() {
         .set({
             margin: 0,
             filename: "lost_and_found_report.pdf",
-            image: { type: 'jpeg', quality: 0.7 },
-            html2canvas: { scale: 4 },
-            jsPDF: { unit: 'mm', format: [170, 105], orientation: 'portrait' } // الأبعاد مقسمة على 2
+            image: { type: 'jpeg', quality: 1 },
+            html2canvas: { scale: 5 },
+            jsPDF: { unit: 'mm', format: [155, 105], orientation: 'portrait' } // الأبعاد مقسمة على 2
         })
         .from(pdfContent)
         .save();
